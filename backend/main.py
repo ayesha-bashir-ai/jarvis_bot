aa750e0514486609d214a5e8e7af18f1e4226dca
Ayesha Bashir <ayesha104bashir@gmail.com>
2026-04-25 12:10:59 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -165,8 +165,5 @@ if os.path.isdir(FRONTEND_DIR):
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "backend.main:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    
