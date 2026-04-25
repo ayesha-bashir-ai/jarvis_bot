c5c9a90ce1f4ca35356ba7375b74819a462296fb
Ayesha Bashir <ayesha104bashir@gmail.com>
2026-04-25 12:15:48 +0500

    Update utils.js

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,9 +1,10 @@
-﻿class Utils {
+class Utils {
     static formatTime() {
         return new Date().toLocaleTimeString();
     }
 
+    // ✅ FIXED: modern safe ID generator
     static generateId() {
-        return Date.now() + "_" + Math.random().toString(36).substr(2, 5);
+        return `${Date.now()}_${Math.random().toString(36).slice(2, 7)}`;
     }
 }
